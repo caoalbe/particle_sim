d49d3c71e060fee2f96aa4d54ad7433db82190f8
Albert Cao <albert.cao.1000@gmail.com>
2025-06-30 13:11:10 -0400

    improved tip readability

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "Vec2f.hpp"
 
 
+const float pi = 3.1415926535;
+
 int main() {
     // Create the main window
     sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({800, 600}), "particle sim");
@@ -58,20 +60,17 @@ int main() {
     sf::CircleShape particle_sprite;
 
     sf::Vertex line_data[6];
-    const float MAX_FIELD_LENGTH = 10.0f;
+    Vec2f tip_vec;
+    const float MAX_ARROW_LENGTH = 25.0f;
+    const float MIN_ARROW_LENGTH = 2.5f;
+    const float ARROW_TIP_PROPORTION = 0.45; 
+    const float ARROW_TIP_ANGLE = 15 * (pi / 180);
     line_data[0].color = sf::Color::Yellow;
     line_data[1].color = sf::Color::Yellow;
     line_data[2].color = sf::Color::Yellow;
     line_data[3].color = sf::Color::Yellow;
     line_data[4].color = sf::Color::Yellow;
     line_data[5].color = sf::Color::Yellow;
-
-    // sf::Vertex tip[2];
-    // tip[0].color = sf::Color::Yellow;
-    // tip[1].color = sf::Color::Yellow;
-
-
-    Vec2f tip_vec;
 
     // Start the simulation loop
     while (window.isOpen()) {
@@ -88,22 +87,27 @@ int main() {
         window.clear();
 
         for (FieldLine& field_line : sim.field_list) {
-            if (field_line.field.length() < 1.0f) { continue; } // field is sufficiently small to skip
-            tip_vec = field_line.field.normalized() * 0.45;
+            if (field_line.field.length() < MIN_ARROW_LENGTH) { continue; }
 
+            // Main field line
             line_data[0].position = static_cast<sf::Vector2f>(field_line.position);
-            if (field_line.field.length() > MAX_FIELD_LENGTH) { 
+            if (field_line.field.length() > MAX_ARROW_LENGTH) { 
                 line_data[1].position = static_cast<sf::Vector2f>(
-                                            field_line.position + MAX_FIELD_LENGTH * field_line.field.normalized());
-                tip_vec *= MAX_FIELD_LENGTH;
+                                            field_line.position + MAX_ARROW_LENGTH * field_line.field.normalized());
+                tip_vec = field_line.field.normalized() * MAX_ARROW_LENGTH * ARROW_TIP_PROPORTION;
             } else {
                 line_data[1].position = static_cast<sf::Vector2f>(
                                             field_line.position + field_line.field);
+                tip_vec = field_line.field * ARROW_TIP_PROPORTION;
             }
+
+            // Left tip of field line
             line_data[2].position = line_data[1].position;
-            line_data[3].position = line_data[2].position + static_cast<sf::Vector2f>(tip_vec.rotated(3.14159 - 0.261799));
+            line_data[3].position = line_data[2].position + static_cast<sf::Vector2f>(tip_vec.rotated(pi - ARROW_TIP_ANGLE));
+            
+            // Right tip of field line
             line_data[4].position = line_data[1].position;
-            line_data[5].position = line_data[4].position + static_cast<sf::Vector2f>(tip_vec.rotated(3.14159 + 0.261799));
+            line_data[5].position = line_data[4].position + static_cast<sf::Vector2f>(tip_vec.rotated(pi + ARROW_TIP_ANGLE));
 
             window.draw(line_data, 6, sf::PrimitiveType::Lines);
         }
